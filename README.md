38f0e5e498a4798700db1b04387f7688c57c61f7
mattp94 <matthieu.puibaraud@isep.fr>
2018-06-06 01:59:52 +0200

    Add remember me option

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 The only **requirement** is to have at least [Node.js v8](https://nodejs.org). Then, make a global installation with [npm](https://www.npmjs.com/) and run it:
 
 ```bash
-misterlike --email <email> --password <password> [--all --show]
+misterlike --email <email> --password <password> [--all --show --remember]
 ```
 
 More information with `--help` option.
--- index.js
+++ index.js
@@ -7,26 +7,26 @@ const like = require('./lib/like')
 const logger = require('./lib/logger')
 const login = require('./lib/login')
 const logout = require('./lib/logout')
-const options = require('./lib/options')
+const { email, password, show } = require('./lib/options')
 
 const main = async () => {
     const browser = await puppeteer.launch({
-        headless: !options.show,
-        executablePath: config.execPath
+        headless: !show,
+        executablePath: config.get('path')
     })
 
     const [page] = await browser.pages()
     await page.setBypassCSP(true)
 
-    if (options.show)
+    if (show)
         await page.setViewport({
-            width: config.viewport.width,
-            height: config.viewport.height
+            width: config.get('viewport.width'),
+            height: config.get('viewport.height')
         })
 
-    page.setDefaultNavigationTimeout(config.timeout)
+    page.setDefaultNavigationTimeout(config.get('timeout'))
 
-    await login(page)
+    await login(page, email, password)
     await like(page)
     await logout(page)
     await browser.close()
